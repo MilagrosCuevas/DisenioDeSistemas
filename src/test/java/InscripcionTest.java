224ba74f977753845c0e8fd270a5138d9ea06f6c
MilagrosCuevas <kcuevasvazquez@frba.utn.edu.ar>
2024-04-03 14:59:35 -0300

    Update InscripcionTest.java

--- src/test/java/InscripcionTest.java
+++ src/test/java/InscripcionTest.java
@@ -1,3 +1,26 @@
-public class InscripcionTest {
+package domain;
 
-}
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+//public class InscripcionTest {
+//    @Test
+//            public void a(){
+//        Alumno pepe= new Alumno();
+//        Materia algoritmos= new Materia();
+//        Materia pdep = new Materia();
+//        pepe.addMateriasAprobadas(pdep);
+//        pepe.addMateriasAprobadas(algoritmos);
+//        pdep.setCorrelativas((List<Materia>) algoritmos);
+//        Inscripcion unaInscripcion= new Inscripcion();
+//        unaInscripcion.setAlumno(pepe);
+//        unaInscripcion.agregarMaterias(pdep);
+//
+//        Assert.assertTrue(unaInscripcion.aprobada());
+//    }
+//
+//}
+
+//Tendría que haber dos clases de equivalencia, la de inscripción aprobada e inscripión no aprobada
